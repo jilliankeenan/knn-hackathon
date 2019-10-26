fc7fe55c84938e24f0dd22ef78e7946c89001ded
cneeson <craigneeson@hotmail.co.uk>
2019-10-26 14:47:23 +0100

    Added skeleton for results page

---
+++ ui/src/components/SearchBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Input } from './styled'
+
+const SearchBar = () => {
+
+    return (<Input type="search" placeholder="Search for something" />);
+}
+
+export default SearchBar;
--- ui/src/containers/result/index.js
+++ ui/src/containers/result/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import mockData from './mock';
+import { Heading } from '../../components/Heading';
+import VideoPlayer from '../../components/VideoPlayer';
+import SearchBar from '../../components/SearchBar';
+import Select from '../../components/Select';
+import { PageContainer, FlexContainer, VideoContainer, TranscriptionPaneContainer, ControlBar, VideoQualityIndicator } from './styled';
 
 const ResultPage = () => {
+    const { videoId } = useParams();
+    const [resultData, setResultData] = useState(mockData);
 
+    console.log({ resultData });
 
-
-    return <h1>This is the result page</h1>;
+    return (
+        <Fragment>
+            <PageContainer>
+                <Heading>{resultData.name}</Heading>
+                <FlexContainer>
+                    <VideoContainer>
+                        <ControlBar>
+                            <SearchBar />
+                        </ControlBar>
+                        <VideoPlayer />
+                        <VideoQualityIndicator>Video Quality Indicator</VideoQualityIndicator>
+                    </VideoContainer>
+                    <TranscriptionPaneContainer>
+                        <ControlBar>
+                            <Select />
+                        </ControlBar>
+                        Transcription Pane
+                    </TranscriptionPaneContainer>
+                </FlexContainer>
+            </PageContainer>
+        </Fragment>
+    );
 };
 
 export default ResultPage;
