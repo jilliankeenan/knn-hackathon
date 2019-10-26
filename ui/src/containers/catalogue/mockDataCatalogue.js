66a1b043cf6d661b937909a923ad1a9bca5b88d7
Jillian Keenan <jilliankeenan1@outlook.com>
2019-10-26 14:56:43 +0100

    merging commits

---
+++ ui/src/containers/catalogue/mockDataCatalogue.js
@@ -0,0 +1,20 @@
+export const catalogueData = [{
+    id: 0,
+    videoName: "The wild west",
+    videoStatus: "Processing",
+},
+{
+    id: 0,
+    videoName: "Checkmate",
+    videoStatus: "Done",
+},
+{
+    id: 0,
+    videoName: "Wild Thing",
+    videoStatus: "Done",
+},
+{
+    id: 0,
+    videoName: "Pretty Woman",
+    videoStatus: "Done",
+}]
--- ui/src/containers/catalogue/styled.js
+++ ui/src/containers/catalogue/styled.js
@@ -1,1 +1,7 @@
 // styled components should go in here and be imported into container
+import  styled from 'styled-components';
+
+export const ImageContainer = styled.img`
+    height: 150px;
+    width: 200px;
+`;
