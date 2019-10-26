17a0e204fd65ed25ca58f1159205ce8df09d6895
Sean Kennedy <sean@automated-intelligence.com>
2019-10-26 18:01:40 +0100

    Added models for response from indexer API.

---
+++ Models/VideoLabel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace knn_hackathon.Models
+{
+    public class VideoLabel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string language { get; set; }
+        public List<Instance> instances { get; set; }
+    }
+}
